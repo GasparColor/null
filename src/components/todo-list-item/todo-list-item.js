bf1f692392349aeb55d2072e36bbf01f3accb05a
Kartashov <kijija23@gmail.com>
2019-06-14 14:28:30 +0500

    todo

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -2,31 +2,74 @@ import React, { Component } from 'react';
 import './todo-list-item.scss'
 
 export default class TodoListItem extends Component {
+
+    /*
+        state = {
+            done: false,
+        }
+    */
+
+    constructor() {
+        super();
+        this.state = {
+            done: false,
+            important: false,
+        };
+    }
+
+    onMarkImportant = () => {
+      this.setState((important) => {
+          return {
+              important: !important,
+          }
+      })
+    };
+
+    onLabelClick = () => {
+
+        this.setState(({done}) =>{
+            return {
+                done: !done,
+            }
+        });
+
+    };
+
+
+
     render() {
 
-        const {label, important = false} = this.props;
+        const {label, onDeleted } = this.props;
+        const { done, important } = this.state;
 
-        const style = {
-            color: important ? 'steelblue' : 'black',
-            fontWeight: important ? 'bold' : 'normal'
-        };
+        let classNames = 'todo-list-item';
+
+        if (done) {
+            classNames += ' done';
+        }
+
+        if (important) {
+            classNames += ' important';
+        }
 
         return (
-            <span className="todo-list-item">
+            <span className={ classNames }>
               <span
+                  onClick={ this.onLabelClick }
                   className="todo-list-item-label"
-                  style={style}
               >
                 {label}
               </span>
 
               <button type="button"
+                      onClick={ this.onMarkImportant }
                       className="btn btn-outline-success btn-sm float-right"
               >
                 <i className="fa fa-exclamation" />
               </button>
 
               <button type="button"
+                      onClick={ onDeleted }
                       className="btn btn-outline-danger btn-sm float-right"
               >
                 <i className="fa fa-trash-o" />
--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -2,12 +2,18 @@ import React from "react";
 import TodoListItem from '../todo-list-item/todo-list-item';
 import './todo-list.scss'
 
-const TodoList = ({ todos}) => {
+const TodoList = ({ todos, onDeleted}) => {
 
     const elements = todos.map((item) => {
+
+       const { id, ...itemProps} = item;
+
        return (
-           <li key={item.id} className="list-group-item">
-               <TodoListItem { ...item}/>
+           <li key={id} className="list-group-item">
+               <TodoListItem
+                   { ...itemProps}
+                   onDeleted={ () =>  onDeleted(id) }
+               />
            </li>
        )
     });
